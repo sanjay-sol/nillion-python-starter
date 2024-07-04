24069a047139e18a190fd1584d69fa418759a098
sanjay-sol <sanjaysirangi@gmail.com>
2024-07-04 19:53:37 +0000

    my new nada program

--- quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
+++ quickstart_complete/nada_quickstart_programs/src/secret_addition_complete.py
@@ -7,6 +7,11 @@ def nada_main():
 
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    new_int = my_int1 + my_int2
+    new_int = my_int1 * my_int2
 
-    return [Output(new_int, "my_output", party1)]
+    output = Output(result, "outpu", party1)
+
+    return [output]
+
+
+
